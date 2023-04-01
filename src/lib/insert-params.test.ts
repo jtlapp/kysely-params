8e4af5a5e52671bab16070341305ae74a6e2ed4e
Joseph T. Lapp <arachnojoe@gmail.com>
2023-04-01 10:56:53 -0400

    test: parameterizing single query with multiple insertions

--- src/lib/insert-params.test.ts
+++ src/lib/insert-params.test.ts
@@ -88,3 +88,63 @@ it('parameterizes inserted strings and numbers with null values', async () => {
     .executeTakeFirst();
   expect(readUser).toEqual({ ...user, id: 1 });
 });
+
+it('parameterizes single query with multiple insertions', async () => {
+  type Params = {
+    nameParam1and2: string;
+    nicknameParam1: string;
+    birthYearParam1: number;
+    birthYearParam2: number;
+  };
+  const user1 = {
+    name: 'John Smith',
+    nickname: 'Johny',
+    handle: 'jsmith1',
+    birthYear: 1990,
+  };
+  const user2 = {
+    name: 'John Smith',
+    nickname: null,
+    handle: 'jsmith2',
+    birthYear: 2000,
+  };
+
+  const parameterized = db
+    .insertInto('users')
+    .parameterize<Params>(({ qb, p }) =>
+      qb
+        .values([
+          {
+            handle: user1.handle,
+            name: p.param('nameParam1and2'),
+            nickname: p.param('nicknameParam1'),
+            birthYear: p.param('birthYearParam1'),
+          },
+          {
+            handle: user2.handle,
+            name: p.param('nameParam1and2'),
+            nickname: user2.nickname,
+            birthYear: user2.birthYear,
+          },
+        ])
+        .returning('id')
+    );
+  const result = await parameterized.execute(db, {
+    nameParam1and2: user1.name,
+    nicknameParam1: user1.nickname,
+    birthYearParam1: user1.birthYear,
+    birthYearParam2: user2.birthYear,
+  });
+
+  expect(result.rows).toEqual([{ id: 1 }, { id: 2 }]);
+
+  const readUsers = await db
+    .selectFrom('users')
+    .selectAll()
+    .where('name', '=', user1.name)
+    .execute();
+  expect(readUsers).toEqual([
+    { ...user1, id: 1 },
+    { ...user2, id: 2 },
+  ]);
+});
