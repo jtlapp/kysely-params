425467548c04db74a6bab39a688d19a7772d5a25
Joseph T. Lapp <arachnojoe@gmail.com>
2023-04-02 14:27:10 -0400

    feat: export delete extension

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import './lib/extensions';
 
 export * from './lib/parameterizer';
+export * from './lib/delete-params';
 export * from './lib/insert-params';
 export * from './lib/select-params';
 export * from './lib/update-params';
